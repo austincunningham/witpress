d4f35c0b51b220c1a8dbab7e9d4c6edf78955973
austincunningham <austincunningham@hotmail.com>
2016-06-24 17:39:48 +0100

    Delete post on blog view now working

--- app/controllers/BlogCtrl.java
+++ app/controllers/BlogCtrl.java
@@ -19,27 +19,22 @@ public class BlogCtrl extends Controller
   public static void post(String Title, String Content)
   {
     Post post = new Post(Title, Content);
-    Logger.info("Before Loop :"+post.Content+" "+post.Title+" "+post.id);
     post.save();
+    Logger.info("Before Loop :"+post.Content+" "+post.Title+" "+post.id);
     List<Post> posts = Post.findAll();
-    Logger.info("posts why -1 " +posts.indexOf(2));
-    
-
     for(int i = 0;i>posts.size();i++)
     {
-      
-      //Logger.info("In loop post id :"+ posts[i].id +" Title :"+posts.Title+" Content :"+p.Content);
       posts.add(post);
-      i++;
       Logger.info("end of Loop :"+post.Content+" "+post.Title+" "+post.id);
     }
-    render("BlogCtrl/index.html", posts);
+    index();
   }
 /**
  * 
  */
-  public static void deletePost(Post post)
+  public static void deletePost(Long id)
   {
+    Post post = Post.findById(id);
     post.delete();
     index();
   }
