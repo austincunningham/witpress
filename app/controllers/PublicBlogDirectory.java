2196a0781b78c1ad5f65e47651a4c0b24ce69701
austincunningham <austincunningham@hotmail.com>
2016-06-28 17:30:06 +0100

    publicBlogs list view setup

--- app/controllers/PublicBlogDirectory.java
+++ app/controllers/PublicBlogDirectory.java
@@ -1,6 +1,18 @@
 package controllers;
 
-public class PublicBlogDirectory
+import play.*;
+import play.mvc.*;
+import java.util.*;
+import models.*;
+import play.mvc.Controller;
+
+public class PublicBlogDirectory extends Controller
 {
+
+  public static void index()
+  {
+    List<User> alluser =  User.findAll();
+    render(alluser);
+  }
 
 }
